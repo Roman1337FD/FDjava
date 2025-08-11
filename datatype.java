8a1bf00703a2483eb9edbcf6c781c01b71060f27
Roman Kannaujiya <roman.kannaujiya_cs24@gla.ac.in>
2025-08-11 11:43:06 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,23 @@
+public class datatype{
+    public static void main(String[] args){
+        int a=25;
+        float b=3.14f;
+        double c=22.56;
+        char d='R';
+        boolean e=true;
+        byte f=100;
+        short s=32000;
+        long l=123456789L;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(s);
+        System.out.println(l);
+
+
+    }
+    
+    }
