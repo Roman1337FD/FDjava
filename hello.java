9d0573cfeef9045e463565158e6629b2d69f81c7
Roman Kannaujiya <roman.kannaujiya_cs24@gla.ac.in>
2025-08-11 10:11:00 +0530

    Delete hello.java

--- hello.java
+++
@@ -1,8 +0,0 @@
-public class hello{
-    public static void main(String[] args){
-        // to print name 
-       System.out.println("Roman");
-        System.out.println("priyanshu");
-        System.out.print("Rahul");
-    }
-}
