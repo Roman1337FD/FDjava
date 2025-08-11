3e5012f8cff4f82e0a0165a8231a6cd99f252e1e
Roman Kannaujiya <roman.kannaujiya_cs24@gla.ac.in>
2025-08-11 11:43:42 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,15 @@
+public class variable{
+    public static void main(String[] args){
+        // int a=5;//Local variable
+        // int b=6;
+        // int $=9;
+        //  System.out.print(a+b+$);
+       
+         int b=6;
+         int $=9;
+         int c=b+$;
+         System.out.print(c);
+
+        
+    }
+}
