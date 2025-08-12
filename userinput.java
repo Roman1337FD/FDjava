ab52a4efcd0ab597514ba364f49217fedddcf345
Roman Kannaujiya <roman.kannaujiya_cs24@gla.ac.in>
2025-08-12 09:34:39 +0530

    Add files via upload

---
+++ userinput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public static void main(String[]args){
+    Scanner ab = new Scanner(System.in);
+    System.out.println("Enter a no.");
+    int a = ab.nextInt();
+    System.out.println(a);
+
+}
+
