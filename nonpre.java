54a1bb04ad28ac9e793bd49d384db26261e767d7
Roman Kannaujiya <roman.kannaujiya_cs24@gla.ac.in>
2025-08-12 09:32:46 +0530

    Add files via upload

---
+++ nonpre.java
@@ -0,0 +1,13 @@
+public class nonpre {
+    public static void main(String[] args){
+        int []b= {3,4,5};
+        int c[]={1,3,7};
+        int a[]={2,4,9};
+        System.out.println(b[1]);
+        System.out.println(c[0]);
+        System.out.println(a[2]);
+
+    }
+
+
+}
